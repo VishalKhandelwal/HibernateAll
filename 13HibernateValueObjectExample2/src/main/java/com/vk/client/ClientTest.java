915d4beb9b2a7771de4d8e6991dd3deb0628e6b3
Vishal Khandelwal <vishalshekhar9316@gmail.com>
2021-04-13 21:49:47 +0530

    Hibernate Value Object Example

---
+++ 13HibernateValueObjectExample2/src/main/java/com/vk/client/ClientTest.java
@@ -0,0 +1,54 @@
+package com.vk.client;
+
+import java.util.Date;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.vk.entities.Employee;
+import com.vk.model.Address;
+import com.vk.util.HibernateUtil;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+	    try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+	    	
+	    	createEmployee(session);
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	  }
+
+	private static void createEmployee(Session session) {
+		session.beginTransaction();
+		Integer id =(Integer)session.save(getEmployee());
+		System.out.println("Employee is created  with Id::"+id);
+		session.getTransaction().commit();
+		
+	}
+	
+	private static Employee getEmployee(){
+		Employee employee= new Employee();
+		employee.setEmployeeName("Barry Bingel");
+		employee.setEmail("barry.cs2017@gmail.com");
+		employee.setSalary(50000.00);
+		employee.setDoj(new Date());
+		
+		Address homeAddress = new Address();
+		homeAddress.setCity("Chennai");
+		homeAddress.setPincode(9087727L);
+		homeAddress.setState("Tamilnadu");
+		homeAddress.setStreet("Park Street");
+		
+		Address officeAddress = new Address();
+		officeAddress.setCity("Pune");
+		officeAddress.setPincode(90877279878L);
+		officeAddress.setState("MH");
+		officeAddress.setStreet("XYZ Street");
+		
+		employee.setHomeAddress(homeAddress);
+		employee.setOfficeAddress(officeAddress);
+		return employee;
+	}
+}
