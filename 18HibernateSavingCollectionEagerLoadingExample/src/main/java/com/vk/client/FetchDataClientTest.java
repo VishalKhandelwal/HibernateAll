6962adb3498921434e06ed51b6eab30a11b354f7
Vishal Khandelwal <vishalshekhar9316@gmail.com>
2021-05-15 18:08:00 +0530

    Eager Loading Example

---
+++ 18HibernateSavingCollectionEagerLoadingExample/src/main/java/com/vk/client/FetchDataClientTest.java
@@ -0,0 +1,33 @@
+package com.vk.client;
+
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.vk.entities.Employee;
+
+import com.vk.util.HibernateUtil;
+
+public class FetchDataClientTest {
+
+	public static void main(String[] args) {
+		
+		Employee employee = null;
+	    try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+	    	
+	    	 employee = session.get(Employee.class,1);
+	    	System.out.println(employee);
+	    	
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	    
+	    if(employee != null)
+    	{
+    		employee.getAddressList().forEach(System.out::println);
+    	}
+	}
+
+	
+}
